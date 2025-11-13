0ecdf440100ab9a2950019d40352b6d3043df312
umair20011 <danishmaniyar2109@gmail.com>
2025-11-13 23:55:54 +0530

    configured H2 DataBase and YAML properties

---
+++ Section2/accounts/src/main/resources/schema.sql
@@ -0,0 +1,21 @@
+CREATE TABLE IF NOT EXISTS `customer` (
+`cutomer_id`  int AUTO_INCREMENT PRIMARY KEY,
+`name` varchar(100) NOT NULL,
+`email` varchar(100) NOT NULL,
+`mobile_number` varchar(20) NOT NULL,
+`created_at` date NOT NULL,
+`created_by` varchar(20) NOT NULL,
+`update_at` date DEFAULT NULL,
+`update_by` varchar(20) DEFAULT NULL
+);
+
+CREATE TABLE IF NOT EXISTS `accounts`(
+`customer_id` int NOT NULL,
+`account_number` int AUTO_INCREMENT PRIMARY KEY,
+`account_type` varchar(100) NOT NULL,
+`branch_address` varchar(200) NOT NULL,
+`created_at` date NOT NULL,
+`created_by` varchar(20) NOT NULL,
+`update_at` date DEFAULT NULL,
+`update_by` varchar(20) DEFAULT NULL
+);
