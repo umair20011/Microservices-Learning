702663b370af0de9d5e983e09fdbec735c4cb1bf
umair20011 <danishmaniyar2109@gmail.com>
2025-11-14 21:13:55 +0530

    written logic for creating account and saving customer and account data in to customer and accounts entity

--- Section2/accounts/src/main/java/com/learning/accounts/service/impl/AccountsServiceImpl.java
+++ Section2/accounts/src/main/java/com/learning/accounts/service/impl/AccountsServiceImpl.java
@@ -1,12 +1,18 @@
 package com.learning.accounts.service.impl;
 
+import com.learning.accounts.constants.AccountsConstants;
 import com.learning.accounts.dto.CustomerDto;
+import com.learning.accounts.entity.Accounts;
+import com.learning.accounts.entity.Customer;
+import com.learning.accounts.mapper.CustomerMapper;
 import com.learning.accounts.repository.AccountsRepository;
 import com.learning.accounts.repository.CustomerRepository;
 import com.learning.accounts.service.IAccountsService;
 import lombok.AllArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.Random;
 
 @Service
 @AllArgsConstructor
@@ -17,6 +23,19 @@ public class AccountsServiceImpl implements IAccountsService {
 
     @Override
     public void createAccount(CustomerDto customerDto) {
-
+        Customer customer = CustomerMapper.mapToCustomer(customerDto,new Customer());
+        Customer savedCustomer=customerRepository.save(customer);
+        accountsRepository.save(createNewAccount(savedCustomer));
     }
+
+    private Accounts createNewAccount(Customer customer){
+        Accounts newAccount=new Accounts();
+        newAccount.setCustomerId(customer.getCustomerId());
+        long randomAccNumber=1000000000L + new Random().nextInt(900000000);
+        newAccount.setAccountNumber(randomAccNumber);
+        newAccount.setAccountType(AccountsConstants.SAVINGS);
+        newAccount.setBranchAddress(AccountsConstants.ADDRESS);
+        return newAccount;
+    }
+
 }
