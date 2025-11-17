533a96c03c4bdccdacccab56134969b723ed66a0
umair20011 <danishmaniyar2109@gmail.com>
2025-11-17 17:26:44 +0530

    added global exception handling method with Exception class

--- Section2/accounts/src/main/java/com/learning/accounts/exception/GlobalExceptionHandler.java
+++ Section2/accounts/src/main/java/com/learning/accounts/exception/GlobalExceptionHandler.java
@@ -13,6 +13,17 @@ import java.time.LocalDateTime;
 @ControllerAdvice
 public class GlobalExceptionHandler {
 
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<ErrorResponseDto> handleGlobalException(Exception exception, WebRequest webRequest){
+        ErrorResponseDto errorResponseDto=new ErrorResponseDto(
+                webRequest.getDescription(false),
+                HttpStatus.INTERNAL_SERVER_ERROR,
+                exception.getMessage(),
+                LocalDateTime.now()
+        );
+        return new ResponseEntity<>(errorResponseDto, HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+
     @ExceptionHandler(ResourceNotFoundException.class)
     public ResponseEntity<ErrorResponseDto> handleResourceNotFoundException(ResourceNotFoundException exception, WebRequest webRequest){
         ErrorResponseDto errorResponseDto=new ErrorResponseDto(
